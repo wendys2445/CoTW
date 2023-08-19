bf19c4dc3829904070271da3ecf3713b01db7c06
wendys2445 <128321397+wendys2445@users.noreply.github.com>
2023-08-19 22:16:44 +0100

    Add files via upload

---
+++ Updater.ps1
@@ -0,0 +1,14 @@
+﻿
+Write-Host "Update Required! Press any Key To Continue..." -ForegroundColor Green
+$x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/wendys2445/CoTW/main/ResetGui.ps1 -OutFile C:\Users\Public\CoTWPopulationResetFiles\ResetGuiNew.ps1
+Remove-Item C:\Users\Public\CoTWPopulationResetFiles\ResetGui.ps1
+Rename-Item -Path "C:\Users\Public\CoTWPopulationResetFiles\ResetGuiNew.ps1" -NewName "C:\Users\Public\CoTWPopulationResetFiles\ResetGui.ps1"
+Get-Process
+for ($i = 1; $i -le 100; $i++ ) {
+    Write-Progress -Activity "Updating" -Status "$i% Complete:" -PercentComplete $i
+    Start-Sleep -Milliseconds 15
+}
+Write-Host "Update Complete! Press Any key To Continue and then relaunch the script to use." -ForegroundColor Green
+$x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Exit
