791f5446f876d4f694b282c8c82a9d033d0b72a0
wendys2445 <128321397+wendys2445@users.noreply.github.com>
2023-08-20 01:31:45 +0100

    Update Updater.ps1

--- Updater.ps1
+++ Updater.ps1
@@ -1,6 +1,7 @@
 ﻿
 Write-Host "Update Required! Press any Key To Continue..." -ForegroundColor Green
 $x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Write-Host "Updating Please Wait..."
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/wendys2445/CoTW/main/ResetGui.ps1 -OutFile C:\Users\Public\CoTWPopulationResetFiles\ResetGuiNew.ps1
 Remove-Item C:\Users\Public\CoTWPopulationResetFiles\ResetGui.ps1
 Rename-Item -Path "C:\Users\Public\CoTWPopulationResetFiles\ResetGuiNew.ps1" -NewName "C:\Users\Public\CoTWPopulationResetFiles\ResetGui.ps1"
