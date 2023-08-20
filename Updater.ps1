2c34e66d15de8f0e9ad734361e14bccdf9c771b0
wendys2445 <128321397+wendys2445@users.noreply.github.com>
2023-08-20 01:43:16 +0100

    Update Updater.ps1

--- Updater.ps1
+++ Updater.ps1
@@ -1,7 +1,8 @@
-﻿
+#Notifying User of Update and asking them to continue﻿
 Write-Host "Update Required! Press any Key To Continue..." -ForegroundColor Green
 $x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 Write-Host "Updating Please Wait..."
+#Getting most recent script from Github and replacing old script
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/wendys2445/CoTW/main/ResetGui.ps1 -OutFile C:\Users\Public\CoTWPopulationResetFiles\ResetGuiNew.ps1
 Remove-Item C:\Users\Public\CoTWPopulationResetFiles\ResetGui.ps1
 Rename-Item -Path "C:\Users\Public\CoTWPopulationResetFiles\ResetGuiNew.ps1" -NewName "C:\Users\Public\CoTWPopulationResetFiles\ResetGui.ps1"
@@ -10,6 +11,7 @@ for ($i = 1; $i -le 100; $i++ ) {
     Write-Progress -Activity "Updating" -Status "$i% Complete:" -PercentComplete $i
     Start-Sleep -Milliseconds 15
 }
+#End of update
 Write-Host "Update Complete! Press Any key To Continue and then relaunch the script to use." -ForegroundColor Green
 $x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 Exit
