502357ab2842cde551c1753ae7f6fcaeb5e082b5
wendys2445 <128321397+wendys2445@users.noreply.github.com>
2023-10-01 18:17:45 +0100

    Update Updater.ps1

--- Updater.ps1
+++ Updater.ps1
@@ -5,8 +5,11 @@ Write-Host "Updating Please Wait..."
 Start-Sleep -Seconds 1
 #Getting most recent script from Github and replacing old script
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/wendys2445/CoTW/main/ResetGui.ps1 -OutFile C:\Users\Public\CoTWPopulationResetFiles\ResetGuiNew.ps1
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/wendys2445/CoTW/main/Start.bat -Outfile C:\Users\Public\CoTWPopulationResetFiles\StartNew.bat
 Remove-Item C:\Users\Public\CoTWPopulationResetFiles\ResetGui.ps1
+Remove-Item C:\Users\Public\CoTWPopulationResetFiles\StartNew.bat
 Rename-Item -Path "C:\Users\Public\CoTWPopulationResetFiles\ResetGuiNew.ps1" -NewName "C:\Users\Public\CoTWPopulationResetFiles\ResetGui.ps1"
+Rename-Item -Path "C:\Users\Public\CoTWPopulationResetFiles\StartNew.bat" -NewName "C:\Users\Public\CoTWPopulationResetFiles\Start.ps1"
 Get-Process
 for ($i = 1; $i -le 100; $i++ ) {
     Write-Progress -Activity "Updating" -Status "$i% Complete:" -PercentComplete $i
