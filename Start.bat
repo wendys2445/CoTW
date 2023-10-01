56f37e21b128a80ffde5154655c8c1c14ad7b829
wendys2445 <128321397+wendys2445@users.noreply.github.com>
2023-10-01 18:14:23 +0100

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -1,2 +1,3 @@
 @echo off
+Powershell -Command Set-ExecutionPolicy unrestricted -Scope CurrentUser
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\Users\Public\CoTWPopulationResetFiles\ResetGui.ps1'"
