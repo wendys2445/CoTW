b3a424c8189236f3715fe272d3747730faea271e
wendys2445 <128321397+wendys2445@users.noreply.github.com>
2023-10-01 18:25:30 +0100

    Update Updater.ps1

--- Updater.ps1
+++ Updater.ps1
@@ -7,7 +7,7 @@ Start-Sleep -Seconds 1
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/wendys2445/CoTW/main/ResetGui.ps1 -OutFile C:\Users\Public\CoTWPopulationResetFiles\ResetGuiNew.ps1
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/wendys2445/CoTW/main/Start.bat -Outfile C:\Users\Public\CoTWPopulationResetFiles\StartNew.bat
 Remove-Item C:\Users\Public\CoTWPopulationResetFiles\ResetGui.ps1
-Remove-Item C:\Users\Public\CoTWPopulationResetFiles\StartNew.bat
+Remove-Item C:\Users\Public\CoTWPopulationResetFiles\Start.bat
 Rename-Item -Path "C:\Users\Public\CoTWPopulationResetFiles\ResetGuiNew.ps1" -NewName "C:\Users\Public\CoTWPopulationResetFiles\ResetGui.ps1"
 Rename-Item -Path "C:\Users\Public\CoTWPopulationResetFiles\StartNew.bat" -NewName "C:\Users\Public\CoTWPopulationResetFiles\Start.bat"
 Get-Process
