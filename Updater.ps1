d167e1ca4adf8be154aa0f03c4020ec2cb4e1127
wendys2445 <128321397+wendys2445@users.noreply.github.com>
2023-10-02 16:18:44 +0100

    Update Updater.ps1

--- Updater.ps1
+++ Updater.ps1
@@ -1,16 +1,17 @@
 #Notifying User of Update and asking them to continueï»¿
 $programfiles = $env:ProgramFiles
+cd $programfiles\Wendys2445\CoTWPopulationFileReset\
 Write-Host "Update Required! Press any Key To Continue..." -ForegroundColor Green
 $x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 Write-Host "Updating Please Wait..."
 Start-Sleep -Seconds 1
 #Getting most recent script from Github and replacing old script
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/wendys2445/CoTW/main/ResetGui.ps1 -OutFile $prgramfiles\Wendys2445\CoTWPopulationFileReset\ResetGuiNew.ps1
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/wendys2445/CoTW/main/Start.bat -Outfile $prgramfiles\Wendys2445\CoTWPopulationFileReset\StartNew.bat
-Remove-Item $prgramfiles\Wendys2445\CoTWPopulationFileReset\ResetGui.ps1
-Remove-Item $prgramfiles\Wendys2445\CoTWPopulationFileReset\Start.bat
-Rename-Item -Path "$prgramfiles\Wendys2445\CoTWPopulationFileReset\ResetGuiNew.ps1" -NewName "$prgramfiles\Wendys2445\CoTWPopulationFileReset\ResetGui.ps1"
-Rename-Item -Path "$prgramfiles\Wendys2445\CoTWPopulationFileReset\StartNew.bat" -NewName "$prgramfiles\Wendys2445\CoTWPopulationFileReset\Start.bat"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/wendys2445/CoTW/main/ResetGui.ps1 -OutFile .\ResetGuiNew.ps1
+
+Remove-Item .\ResetGui.ps1
+
+Rename-Item -Path ".\ResetGuiNew.ps1" -NewName ".\ResetGui.ps1"
+
 Get-Process
 for ($i = 1; $i -le 100; $i++ ) {
     Write-Progress -Activity "Updating" -Status "$i% Complete:" -PercentComplete $i
